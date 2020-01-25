6eaf4bd3d38348ffd9cb799dd94319bfb371d79d
lemospadilha <dlemos.padilha@gmail.com>
2020-01-25 12:05:19 -0300

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import Header from './components/Header'
-import Main from './pages/main';
-
-import './styles.css';
+import React from "react";
+import Header from "./components/Header";
+import Routes from "./routes";
+import "./styles.css";
 
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
