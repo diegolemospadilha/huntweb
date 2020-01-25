b0db122d4f61457a8b161e3cd41d02004723291c
lemospadilha <dlemos.padilha@gmail.com>
2020-01-25 00:31:29 -0300

    Aprimorando estilo componente main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/Header'
+import Main from './pages/main';
 
-import './styles.css'
+import './styles.css';
 
 const App = () => (
-  <Header />
-)
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+);
 
 export default App;
